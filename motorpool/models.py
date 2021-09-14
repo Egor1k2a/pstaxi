972b98fb2da528777deef063c06acd124272b96d
Егор Кеньков <kenkov2045@yandex.ru>
2021-09-14 20:58:23 +0300

    Model Auto

--- motorpool/models.py
+++ motorpool/models.py
@@ -1,1 +1,36 @@
 from django.db import models
+
+
+class Brand(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название бренда')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Бренд'
+        verbose_name_plural = 'Бренды'
+
+
+class Auto(models.Model):
+    AUTO_CLASS_ECONOMY = 'e'
+    AUTO_CLASS_COMFORT = 'c'
+    AUTO_CLASS_BUSINESS = 'b'
+
+    AUTO_CLASS_CHOICES = [
+        (AUTO_CLASS_ECONOMY, 'economy'),
+        (AUTO_CLASS_COMFORT, 'comfort'),
+        (AUTO_CLASS_BUSINESS, 'business'),
+    ]
+
+    number = models.CharField(max_length=15)
+    description = models.TextField(max_length=500, default='', blank=True)
+    year = models.PositiveSmallIntegerField(null=True)
+    auto_class = models.CharField(max_length=1, null=True, choices=AUTO_CLASS_CHOICES, default=AUTO_CLASS_ECONOMY)
+
+    def __str__(self):
+        return self.number
+
+    class Meta:
+        verbose_name_plural = 'Автомобили'
+        verbose_name = 'Автомобиль'
