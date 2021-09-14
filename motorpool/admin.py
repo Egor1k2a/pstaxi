45220e8bd7f8432221ce36322780df9545eda8ac
Егор Кеньков <kenkov2045@yandex.ru>
2021-09-14 15:31:31 +0300

    tests

--- motorpool/admin.py
+++ motorpool/admin.py
@@ -1,7 +1,1 @@
 from django.contrib import admin
-from . import models
-
-
-@admin.register(models.Brand)
-class BrandAdmin(admin.ModelAdmin):
-    pass
--- motorpool/models.py
+++ motorpool/models.py
@@ -1,12 +1,1 @@
 from django.db import models
-
-
-class Brand(models.Model):
-    title = models.CharField(max_length=100, verbose_name='Название бренда')
-
-    class Meta:
-        verbose_name = 'Бренд'
-        verbose_name_plural = 'Бренды'
-
-    def __str__(self):
-        return self.title
